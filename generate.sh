043e9d79b6895a7185c7bff678b1088a15d611eb
Manas Sivakumar <manas23601@gmail.com>
2023-06-12 20:04:32 +0530

    f;

--- generate.sh
+++ generate.sh
@@ -12,7 +12,7 @@ fi
 count=$(jq '.models | length' config.json)
 
 # Generate docker-compose.yaml file
-printf "version: '3'\nservices:\n" > docker-compose-generated.yaml
+printf "version: '3'\nservices:\n" > docker-compose.yaml
 
 # Generate Nginx configuration file
 printf "events {}\n\nhttp {\n    server {\n        server_name ${DOMAIN_NAME};\n" > "${DOMAIN_NAME}.conf.d"
@@ -36,15 +36,15 @@ for ((i=0; i<$count; i++)); do
 
 
     # Add service details to docker-compose.yaml
-    printf "  ${serviceName}:\n    build:\n      context: ${modelBasePath}\n    ports:\n      - ${exposedPort}:${containerPort}\n" >> docker-compose-generated.yaml
+    printf "  ${serviceName}:\n    build:\n      context: ${modelBasePath}\n    ports:\n      - ${exposedPort}:${containerPort}\n" >> docker-compose.yaml
 
     # Add environment variables to docker-compose.yaml
     if [[ ${#environment[@]} -gt 0 ]]; then
-        printf "    environment:\n" >> docker-compose-generated.yaml
+        printf "    environment:\n" >> docker-compose.yaml
     fi
     for key in "${environment[@]}"; do
         value=`jq -r '.models['$i'].environment["'$key'"]' config.json`
-        printf "      - ${key}=${value}\n" >> docker-compose-generated.yaml
+        printf "      - ${key}=${value}\n" >> docker-compose.yaml
     done
 done
 
